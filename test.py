7ae4f50cbcfc173cb0a17725a4c63886fb170d8c
drinkmorehotwater <941742707@qq.com>
2023-05-04 17:20:52 +0800

    代码本地化

--- test.py
+++ test.py
@@ -59,11 +59,11 @@ os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 
 dataset = args.dataset
 if dataset == 'sysu':
-    data_path = '../Datasets/SYSU-MM01/ori_data/'
+    data_path = '/home/zjy/dataset/SYSU-MM01/'
     n_class = 395
     test_mode = [1, 2]
 elif dataset =='regdb':
-    data_path = '../Datasets/RegDB/'
+    data_path = '/home/chrisd/home/dataset/RegDB/'
     n_class = 206
     test_mode = [2, 1]
  
